8dbe799ef25fcddd5ee1d3181b31a2eda97b0aed
alphaqu <yan.gyunashyan@gmail.com>
2022-01-04 15:10:35 +0100

    tiles and walls

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
-use crate::asset::Asset;
-use indicatif::{ProgressBar, ProgressStyle};
+use image::ImageError;
+use image::imageops::FilterType;
+
+use crate::asset::{collect_assets, ResourceManager, ResourcePath};
+use crate::progress_bar::Progress;
+use crate::xnb::XNBFile::Texture;
 
 mod asset;
 mod util;
 mod xnb;
+mod mapper;
+mod registry;
+mod progress_bar;
 
 fn main() {
     let cwd = std::env::current_dir().expect("Could not access current working directory");
@@ -11,13 +18,49 @@ fn main() {
         panic!("Not launched from Terraria's \"Content\" directory.")
     }
 
-    let mut progress_bar = ProgressBar::new(1).with_style(
-        ProgressStyle::default_bar()
-            .template("{msg} {wide_bar:.cyan} {pos}/{len}")
-            .progress_chars("=> "),
-    );
+    let mut progress_bar = Progress::new();
 
-    let assets = asset::collect_assets(&cwd, &mut progress_bar);
+    let out = cwd.join("rustaria");
+    let sprite = out.join("sprite");
 
-    for x in assets {}
+    let manager = collect_assets(&cwd, &mut progress_bar);
+
+    let tile = sprite.join("tile");
+    std::fs::create_dir_all(&tile).unwrap();
+
+    for id in progress_bar.iter(registry::BLOCK_TILES.iter(), "Mapping Tiles") {
+        if let Some(sprite) = manager.get_sprite(ResourcePath::Tile(id.0)) {
+            let sprite = mapper::remap_tile(sprite);
+            let result = sprite.save(tile.join(format!("{}.png", id.1)));
+
+            result.map_err(|error| {
+                progress_bar.print(format!("Failed to export {} {}", id.1, error));
+            }).unwrap();
+        }
+    }
+
+    let wall = sprite.join("wall");
+    std::fs::create_dir_all(&wall).unwrap();
+
+    for id in progress_bar.iter(registry::WALLS.iter(), "Mapping Walls") {
+        if let Some(sprite) = manager.get_sprite(ResourcePath::Wall(id.0)) {
+            let sprite = mapper::remap_wall(sprite);
+            let result = sprite.save(wall.join(format!("{}.png", id.1)));
+
+            result.map_err(|error| {
+                progress_bar.print(format!("Failed to export {} {}", id.1, error));
+            }).unwrap();
+        }
+    }
+
+
+    //let test = sprite.join("test");
+    //std::fs::create_dir_all(&test).unwrap();
+    //for i in progress_bar.iter(1..5124, "Mapping Items") {
+    //    if let Some(sprite) =  manager.get_sprite(ResourcePath::Item(i)) {
+    //        let sprite = image::imageops::resize(&sprite, sprite.width() / 2, sprite.height() / 2, FilterType::Nearest);
+    //        sprite.save(test.join(format!("{}.png", i)));
+    //    }
+    //}
 }
+
---
+++ src/mapper.rs
@@ -0,0 +1,108 @@
+use crate::asset::Sprite;
+
+pub struct Layout {
+    tile_size: u32,
+    tile_padding: u32,
+    sprite: Sprite,
+}
+
+impl Layout {
+    pub fn remap(&mut self, source: &Layout, from: (u32, u32), to: (u32, u32)) {
+        let scale = source.tile_size as f32 / self.tile_size as f32;
+        if scale != scale.round() {
+            panic!("The quotient of the source by the scale is not equal to the target. ");
+        }
+        let scale = scale as u32;
+
+        let source_offset = (source.tile_size + source.tile_padding);
+        let from_pos = (from.0 * source_offset, from.1 * source_offset);
+
+        let target_offset = (self.tile_size + self.tile_padding);
+        let to_pos = (to.0 * target_offset, to.1 * target_offset);
+
+        for y in 0..self.tile_size {
+            for x in 0..self.tile_size {
+                self.sprite.put_pixel(to_pos.0 + x, to_pos.1 + y, *source.sprite.get_pixel(from_pos.0 + (x * scale), from_pos.1 + (y * scale)));
+            }
+        }
+    }
+}
+
+pub fn remap_tile(sprite: Sprite) -> Sprite {
+    let source = Layout { tile_size: 16, tile_padding: 2, sprite };
+    let mut target = Layout { tile_size: 8, tile_padding: 0, sprite: Sprite::new(32 * 3, 40) };
+
+    // v = variant
+    for v_raw in 0..3 {
+        let v = v_raw as u32;
+        let variant_offset = v * 4;
+        target.remap(&source, (0, v), (0 + variant_offset, 3));
+        target.remap(&source, (4, v), (0 + variant_offset, 4));
+        target.remap(&source, (5, v), (2 + variant_offset, 0));
+        target.remap(&source, (9, v), (1 + variant_offset, 3));
+        target.remap(&source, (10, v), (2 + variant_offset, 3));
+        target.remap(&source, (11, v), (2 + variant_offset, 4));
+        target.remap(&source, (12, v), (1 + variant_offset, 4));
+        target.remap(&source, (9 + v, 3), (1 + variant_offset, 0));
+        target.remap(&source, (6 + v, 4), (3 + variant_offset, 0));
+        target.remap(&source, (6 + v, 0), (1 + variant_offset, 1));
+        target.remap(&source, (6 + v, 1), (2 + variant_offset, 1));
+        target.remap(&source, (6 + v, 3), (1 + variant_offset, 2));
+        target.remap(&source, (6 + v, 2), (2 + variant_offset, 2));
+        target.remap(&source, (v + 1, 0), (0 + variant_offset, 1));
+        target.remap(&source, (v + 1, 1), (0 + variant_offset, 0));
+        target.remap(&source, (v + 1, 2), (0 + variant_offset, 2));
+        target.remap(&source, (v * 2, 3), (3 + variant_offset, 1));
+        target.remap(&source, (v * 2, 4), (3 + variant_offset, 2));
+        target.remap(&source, ((v * 2) + 1, 3), (3 + variant_offset, 3));
+        target.remap(&source, ((v * 2) + 1, 4), (3 + variant_offset, 4));
+    };
+
+    target.sprite
+}
+
+pub fn remap_wall(sprite: Sprite) -> Sprite {
+    let source = Layout { tile_size: 32, tile_padding: 4, sprite };
+    let mut target = Layout { tile_size: 16, tile_padding: 0, sprite: Sprite::new(8 * 6, 8 * 2) };
+    for v_raw in 0..3 {
+        let v = v_raw as u32;
+        // 3x3 grid copy
+        target.remap(&source, (9 + v, 3), (v, 0));
+    };
+
+    target.sprite
+}
+
+
+pub fn remap_wall_full(sprite: Sprite) -> Sprite {
+    let source = Layout { tile_size: 32, tile_padding: 4, sprite };
+    let mut target = Layout { tile_size: 16, tile_padding: 0, sprite: Sprite::new(32 * 3 * 2, 40 * 2) };
+
+    // v = variant
+    for v_raw in 0..3 {
+        let v = v_raw as u32;
+        let variant_offset = v * 4;
+        target.remap(&source, (0, v), (0 + variant_offset, 3));
+        target.remap(&source, (4, v), (0 + variant_offset, 4));
+        target.remap(&source, (5, v), (2 + variant_offset, 0));
+        target.remap(&source, (9, v), (1 + variant_offset, 3));
+        target.remap(&source, (10, v), (2 + variant_offset, 3));
+        target.remap(&source, (11, v), (2 + variant_offset, 4));
+        target.remap(&source, (12, v), (1 + variant_offset, 4));
+        target.remap(&source, (9 + v, 3), (1 + variant_offset, 0));
+        target.remap(&source, (6 + v, 4), (3 + variant_offset, 0));
+        target.remap(&source, (6 + v, 0), (1 + variant_offset, 1));
+        target.remap(&source, (6 + v, 1), (2 + variant_offset, 1));
+        target.remap(&source, (6 + v, 3), (1 + variant_offset, 2));
+        target.remap(&source, (6 + v, 2), (2 + variant_offset, 2));
+        target.remap(&source, (v + 1, 0), (0 + variant_offset, 1));
+        target.remap(&source, (v + 1, 1), (0 + variant_offset, 0));
+        target.remap(&source, (v + 1, 2), (0 + variant_offset, 2));
+        target.remap(&source, (v * 2, 3), (3 + variant_offset, 1));
+        target.remap(&source, (v * 2, 4), (3 + variant_offset, 2));
+        target.remap(&source, ((v * 2) + 1, 3), (3 + variant_offset, 3));
+        target.remap(&source, ((v * 2) + 1, 4), (3 + variant_offset, 4));
+    };
+
+    target.sprite
+}
